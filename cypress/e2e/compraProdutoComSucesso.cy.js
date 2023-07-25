15d41594ef5b53657246dc8226546253fb59ff50
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-07-25 13:51:22 -0400

    update

--- cypress/e2e/compraProdutoComSucesso.cy.js
+++ cypress/e2e/compraProdutoComSucesso.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Teste E2E - Realizando a compra dos produtos com sucesso!', () => {
-    it('Fluxo de Compras do Produto', () => {
+    it('Fluxo Fechado de Compras do Produto', () => {
         cy.visit("https://www.saucedemo.com/v1/index.html")
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
@@ -11,6 +11,45 @@ describe('Teste E2E - Realizando a compra dos produtos com sucesso!', () => {
         cy.get('.product_sort_container').select('Price (low to high)')
 
         cy.get(':nth-child(1) > .inventory_item_label').should('contain', 'Sauce Labs Onesie')
+        cy.get(':nth-child(2) > .inventory_item_label').should('contain', 'Sauce Labs Bike Light')
+        cy.get(':nth-child(3) > .inventory_item_label').should('contain', 'Sauce Labs Bolt T-Shirt')
+
+        cy.contains('Sauce Labs Onesie').click()
+        cy.get('.btn_primary').click()
+        cy.get('.inventory_details_back_button').click()
+
+        cy.contains('Sauce Labs Bike Light').click()
+        cy.get('.btn_primary').click()
+        cy.get('.inventory_details_back_button').click()
+
+        cy.contains('Sauce Labs Bolt T-Shirt').click()
+        cy.get('.btn_primary').click()
+        cy.get('.inventory_details_back_button').click()
+
+
+        // Checagem de quantida de produtos
+
+        cy.get('.fa-layers-counter').should('have.text', '3').click()
+
+        cy.get('.btn_action').click()
+
+        cy.get('[data-test="firstName"]').type('PrimeiroNome')
+
+        cy.get('[data-test="lastName"]').type('SegundoNome')
+
+        cy.get('[data-test="postalCode"]').type('xxxxx-xxx')
+
+        cy.get('.btn_primary').click()
+
+        cy.get('.summary_total_label').should('have.text', 'Total: $36.69')
+
+        cy.get('.btn_action').click()
+
+        cy.get('.complete-header').should('have.text', 'THANK YOU FOR YOUR ORDER')
+
+
+
+
     });
 
 });
