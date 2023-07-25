a6a73c3714b0fc0a90d3b45fdf80c643f89b22ce
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-07-25 16:09:19 -0400

    update

--- cypress/e2e/compraProdutoComSucesso.cy.js
+++ cypress/e2e/compraProdutoComSucesso.cy.js
@@ -2,10 +2,7 @@
 
 describe('Teste E2E - Realizando a compra dos produtos com sucesso!', () => {
     it('Fluxo Fechado de Compras do Produto', () => {
-        cy.visit("https://www.saucedemo.com/v1/index.html")
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('#login-button').click()
+        cy.login('standard_user', 'secret_sauce')
         cy.get('.product_label').should('contain', 'Products')
 
         cy.get('.product_sort_container').select('Price (low to high)')
